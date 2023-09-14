56edd9d4f0f730e22adb91638b2362b92fa13307
Cees de Groot <cg@cdegroot.com>
2023-09-13 22:31:22 -0400

    Basic build script

--- default.nix
+++ default.nix
@@ -6,24 +6,30 @@ let
 
   pname = "nixfra_phx";
   version = "0.0.1";
+  sha256 = "@@GIT_SHA256@@";
 
-  src =  ./.;
+  src =  pkgs.fetchgit {
+    inherit sha256;
+    url = "@@GIT_URL@@";
+    rev = "@@GIT_REV@@";
+  };
 
   # if using mix2nix you can use the mixNixDeps attribute
-  mixFodDeps = packages.fetchMixDeps {
-    pname = "mix-deps-${pname}";
-    inherit src version;
-    # nix will complain and tell you the right value to replace this with
-    sha256 = "sha256-PmFAyEA8JigeTgApVrjvc8Ig+aTNGEgpIMFVFX3GEOc=";
-    mixEnv = "prod"; # default is "prod", when empty includes all dependencies, such as "dev", "test".
+  mixNixDeps = import ./mix_deps.nix {
+    inherit beamPackages lib;
+  };
+  #mixFodDeps = packages.fetchMixDeps {
+    #pname = "mix-deps-${pname}";
+    #inherit src version sha256;
+    #mixEnv = "prod"; # default is "prod", when empty includes all dependencies, such as "dev", "test".
     # if you have build time environment variables add them here
     # MY_ENV_VAR="my_value";
-  };
+  #};
 
   #nodeDependencies = (pkgs.callPackage ./assets/default.nix { }).shell.nodeDependencies;
 
 in packages.mixRelease {
-  inherit src pname version mixFodDeps;
+  inherit src pname version mixNixDeps;
   # if you have build time environment variables add them here
   #MY_ENV_VAR="my_value";
 
---
+++ nix/build-package.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+base=$(cd $(dirname $0)/..; pwd)
+cd $base
+
+set -e
+
+WORK=$(mktemp -d)
+
+#trap "rm -rf $WORK" 0
+
+# Collect info
+git_url="file:///$PWD"
+git_json=$(nix-prefetch-git --url "$git_url" --rev "$(git rev-parse --verify HEAD)" --quiet)
+git_rev=$(echo "$git_json" | jq '.rev' -r)
+git_sha256=$(echo "$git_json" | jq '.sha256' -r)
+
+sed \
+    -e "s,@@GIT_URL@@,$git_url," \
+    -e "s,@@GIT_REV@@,$git_rev," \
+    -e "s,@@GIT_SHA256@@,$git_sha256," \
+    <default.nix >$WORK/default.nix
+
+nix-shell -p mix2nix --run mix2nix >$WORK/mix_deps.nix
+
+cd $WORK
+nix-build .
