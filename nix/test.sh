6e1f5acc129df32d9eca07aaa0a79420d3f593e0
Cees de Groot <cg@cdegroot.com>
2023-09-27 22:38:13 -0400

    Use Nix packages for hex/rebar3

--- nix/test.sh
+++ nix/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i bash -p elixir
+#! nix-shell -i bash -p elixir beamPackages.hex beamPackages.rebar3
 
 base=$(cd $(dirname $0)/..; pwd)
 cd $base
@@ -7,9 +7,6 @@ cd $base
 set -e
 
 export MIX_ENV=test
-
-mix local.hex --if-missing
-mix local.rebar --if-missing
 
 mix deps.get
 mix test
